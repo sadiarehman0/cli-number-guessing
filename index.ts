74de1c765e94a072907178dbb97b60c230c5ffdf
sadiarehman0 <sadia.rehman1986@gmail.com>
2024-04-28 21:54:40 -0700

    spelling correction

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@
 import inquirer from "inquirer";
 const randomnumber = Math.floor(Math.random() * 6 + 1);
 
-console.log("welcome to number guwessing game");
+console.log("welcome to number guessing game");
 
  const answers = await inquirer.prompt([
      {
